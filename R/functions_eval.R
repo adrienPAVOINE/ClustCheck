f33cccdb3f2f59d02506b73c3bfd87dee1627a31
Fabthered <fab56@hotmail.com>
2020-11-30 12:41:15 +0100

    Added corr + se functions

--- R/functions_eval.R
+++ R/functions_eval.R
@@ -16,7 +16,7 @@
 #' @import ggplot2
 #' @examples
 #library(ggplot2)
-plottvalue <- function(obj){ # devra appeler un objet de la classe plutot - sera à modifier
+plottvalue <- function(obj){
   table <- TValueTable.NumDataset(obj)
   p <- ncol(table)
   variables <- rownames(table)
@@ -31,4 +31,52 @@ plottvalue <- function(obj){ # devra appeler un objet de la classe plutot - sera
     facet_wrap(vars(ind))
 }
 
+# ------------------------------------------------------------------------- #
+# Plot of size effect
+# ------------------------------------------------------------------------- #
+
+#' plotsizeeff
+#'
+#' @param obj an object of class NumDataset
+#'
+#' @return
+#' @export
+#' @import ggplot2
+#' @examples
+#library(ggplot2)
+plotsizeeff <- function(obj){
+  table <- EffectSizeTable.NumDataset(obj)
+  p <- ncol(table)
+  variables <- rownames(table)
+  SE_table <- stack(as.data.frame(table))
+  SE_table$variables <- rep(variables,p)
+  # Visualisation
+  ggplot2::ggplot(SE_table, aes(x=variables, y=values)) +
+    geom_col() +
+    coord_flip() +
+    facet_wrap(vars(ind))
+}
+
+# ------------------------------------------------------------------------- #
+# Plot of correlations
+# ------------------------------------------------------------------------- #
+
+#' plotcorr
+#'
+#' @param obj an object of class NumDataset
+#'
+#' @return
+#' @export
+#' @import ggplot2
+#' @examples
+#library(ggplot2)
+plotcorr <- function(obj){
+  table <- sort(Corr_ratios.NumDataset(obj), decreasing=T)
+  p <- min(12,ncol(table))
+  print(p)
+  corr <- as.data.frame(table[1:p])
+  colnames(corr) <- "values"
+  # Visualisation
+  ggplot(corr, aes(x=reorder(rownames(corr), -values), y=values)) + geom_col()
+}
 
