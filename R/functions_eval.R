1ec18b8289a1a642475a018d5c3572d7e3c51c72
Fabthered <fab56@hotmail.com>
2020-11-30 11:16:51 +0100

    modified

--- R/functions_eval.R
+++ R/functions_eval.R
@@ -7,21 +7,28 @@
 # Plot of t-values
 # ------------------------------------------------------------------------- #
 
-#' plot.t
+#' plotvalue
 #'
-#' @param tvalue a Table of test value
+#' @param Tvalue_table a Table of test value
 #'
 #' @return
 #' @export
 #' @import ggplot2
 #' @examples
-plotvalue <- function(tvalue){
-  # t values table needs a particular format for the function to operation
-  # i.e. 3 columns with variable (variable), t-value (t), cluster name (group)
-  ggplot2::ggplot(tvalue, aes(x=variable, y=t)) +
+
+plotvalue <- function(Tvalue_table){ # devra appeler un objet de la classe plutot - sera à modifier
+  p <- ncol(Tvalue_table)
+  print(p)
+  variables <- rownames(Tvalue_table)
+  Tvalue_table <- stack(as.data.frame(Tvalue_table))
+  Tvalue_table$variables <- rep(variables,p)
+  # Visualisation
+  ggplot2::ggplot(Tvalue_table, aes(x=variables, y=values)) +
     geom_col() +
     geom_hline(yintercept = -2, linetype="dashed", size=0.5, color="red") +
     geom_hline(yintercept = 2, linetype="dashed", size=0.5, color="red") +
     coord_flip() +
-    facet_wrap(vars(group))
+    facet_wrap(vars(ind))
 }
+
+
